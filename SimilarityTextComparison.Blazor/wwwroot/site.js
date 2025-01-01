b706d63287894ae972eb57abcc9c0f4eeddc29f9
Gianluigi Salvi <g.salvi@lottomatica.com>
2025-01-01 15:12:53 +0100

    Ottimizzati layout, evidenziazioni e gestione input nei componenti principali. Aggiunta ComparisonDashboard e funzionalità di stampa con `site.js` e JSInterop.

---
+++ SimilarityTextComparison.Blazor/wwwroot/site.js
@@ -0,0 +1,13 @@
+﻿window.PrintJs = {
+    printElement: function (elementId) {
+        var printContents = document.getElementById(elementId).innerHTML;
+        var originalContents = document.body.innerHTML;
+
+        document.body.innerHTML = printContents;
+
+        window.print();
+
+        document.body.innerHTML = originalContents;
+        window.location.reload(); // Ricarica la pagina per ripristinare lo stato
+    }
+};
